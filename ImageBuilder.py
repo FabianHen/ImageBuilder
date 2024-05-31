99b77913ef8e0d1bac65137a6230b324ac0443cf
Fabian H <hensslerfabian@gmail.com>
2024-06-01 00:20:02 +0200

    Add comments, Fix rescaling, Add Test

--- ImageBuilder.py
+++ ImageBuilder.py
@@ -6,90 +6,119 @@ import numpy as np
 from PIL import Image
 
 
+# constants used for coloring console outputs
+ANSIRED = '\033[91m'
+STANDARD = '\033[0m'
+ANSIGREEN = '\033[92m'
+
+def green() -> str:
+    return ANSIGREEN + "█" + STANDARD
+
+def red() -> str:
+    return ANSIRED + "█" + STANDARD
+
 def find_picture(rgb, imageList) -> Image:
     best = (None, 765)
     for ((x, y, z), image) in imageList:
+        # Searches for the Image witth the lowest offset of RGB values
         offset = abs(rgb[0] - x) + abs(rgb[1] - y) + abs(rgb[2] - z)
         if offset < best[1]:
             best = (image, offset)
     return best[0]
 
-
-def create_image_list(folder) -> list[tuple[tuple, Image]]:
+def create_image_list(folder) -> list[tuple[tuple, Image.Image]]:
     image_list = []
-
+    # Only accept certain Image file formats
     filetypes = ("png", "jpg", "jpeg", "webp")
+    # Iterate through given Directory
     for filename in os.listdir(folder):
         if filename.endswith(filetypes):
             filepath = os.path.join(folder, filename)
             img = Image.open(filepath).convert('RGB')
             avg_color = tuple(np.array(img).mean(axis=(0, 1)))
+            # Creates a List that contains all Images and their RGB Values in a Tupel
             image_list.append((avg_color, img))
     print("time for creating list", time.time() - start, "seconds")
-    print(len(image_list))
+    print("Length of Imagelist:", len(image_list))
     return image_list
 
 
 def create_image(amount: int, image: Image, width: float, height: float):
     img = Image.new('RGB', image.size)
     imageList = create_image_list(sys.argv[3])
-    lastImage: (Image, (int, int, int)) = (None, (0, 0, 0))
-    lastOffset = 765
+    lastImage: tuple[Image.Image, tuple[int, int, int]] = (None, (0, 0, 0))
+    first: bool = True
+    # Vertical loop
     for i in range(0, amount):
         jRange = None
+        # Creating a "Zick-Zack" for the horizontal loop
         if i % 2 == 1:
             jRange = range(0, amount)
         else:
             jRange = range(amount, 0, -1)
-        string = ""
+        statusString: str = ""
+        # Horizontal loop
         for j in jRange:
+            # Calculating the current pixel
             left = int(j * width)
             upper = int(i * height)
             right = int((j + 1) * width)
             lower = int((i + 1) * height)
             box = (left, upper, right, lower)
+            # Geting the current pixel from the overall image
             pixel = image.crop(box)
             pixelColor = tuple(np.array(pixel).mean(axis=(0, 1)))
+            # Calculating the RGB offset from the last used Image
             off_x = abs(lastImage[1][0] - pixelColor[0])
             off_y = abs(lastImage[1][1] - pixelColor[1])
             off_z = abs(lastImage[1][2] - pixelColor[2])
             off = off_x + off_y + off_z
-            if abs(lastOffset - off) > 10:
+            # Check if the current Pixels RGB values are significantly different to the last
+            if off > 10 or first:
+                # Find best Image for this Pixel
+                first = False
                 pixelImage: Image = find_picture(pixelColor, imageList)
-                lastOffset = off
                 lastImage = (pixelImage, pixelColor)
                 pixelImage = pixelImage.resize((box[2] - box[0], box[3] - box[1]))
                 img.paste(pixelImage, box)
-                string = string + str(0)
+                statusString = statusString + red()
             else:
+                # Use last image 
                 resizedLast = lastImage[0].resize((box[2] - box[0], box[3] - box[1]))
                 img.paste(resizedLast, box)
-                string = string + str(1)
-        print(string)
+                statusString = statusString + green()
+        print(statusString)
     img.show()
 
 
 
 def main():
-    ansiRed = '\033[91m'
-    standard = '\033[0m'
-    green = '\033[92m'
+    # Check if input was correct
     if len(sys.argv) < 4:
-        print(ansiRed + "ERROR: Missing Parameters:", 4 - len(sys.argv), standard)
+        print(ANSIRED + "ERROR: Missing Parameters:", 4 - len(sys.argv), STANDARD)
         return
     if not os.path.isfile(sys.argv[1]):
-        print(ansiRed + "ERROR: " + sys.argv[1] + "is not a file!" + standard)
+        print(ANSIRED + "ERROR: " + sys.argv[1] + "is not a file!" + STANDARD)
         return
     if not os.path.isdir(sys.argv[3]):
-        print(ansiRed + "ERROR: " + sys.argv[3] + "is not a directory!" + standard)
+        print(ANSIRED + "ERROR: " + sys.argv[3] + "is not a directory!" + STANDARD)
         return
     im = Image.open(sys.argv[1]).convert('RGB')
     amount = int(sys.argv[2])
     width, height = im.size
     pixelWidth = width / amount
     pixelHeight = height / amount
-    if pixelWidth <= 10 or pixelHeight <= 10:
-        im.resize((int(10 * pixelWidth), int(10 * pixelHeight)))
+    # Rescale the Image, if the Pixels get to small. Scaling keeps the aspec ratio
+    resized: bool = False
+    if pixelWidth < 10:
+        resized = True
+        im = im.resize((int(10 * amount), int(10 * amount * height/width)))
+        print("Not wide enough - Resized")
+    if pixelHeight < 10:
+        resized = True
+        im = im.resize((int(10 * amount * width/height), int(10 * amount)))
+        print("Not high enough - Resized")
+    if resized:
         width, height = im.size
         pixelWidth = width / amount
         pixelHeight = height / amount
@@ -97,7 +126,7 @@ def main():
           + ")" ' "Pixels":')
     create_image(amount, im, pixelWidth, pixelHeight)
     time_needed = time.time() - start
-    print(green+"Process finished after", time_needed, "seconds" + standard)
+    print(ANSIGREEN + "Process finished after", time_needed, "seconds" + STANDARD)
 
 
 start = time.time()
---
+++ Test/Test1/Test1.md
@@ -0,0 +1,18 @@
+# Linear Test
+* Threshhold: 10
+* Time to create List: 1.8754 - 1.9602s
+
+## Test 1: Fit
+* Time (without prints): 64.5124s<br><br>
+![result Image 1]("https://github.com/FabianHen/ImageBuilder/blob/main/Test/Test1/result1.png")<br><br>
+![result Calculation Image 1 ]("https://github.com/FabianHen/ImageBuilder/blob/main/Test/Test1/result1_calc.png")
+
+## Test 2: Not High Enough
+* Time (without prints): 20.8374s<br><br>
+![result Image 2]("https://github.com/FabianHen/ImageBuilder/blob/main/Test/Test1/result2.png")<br><br>
+![result Calculation Image 2]("https://github.com/FabianHen/ImageBuilder/blob/main/Test/Test1/result2_calc.png")
+
+## Test 3: Not Wide Enough
+* Time (without prints): 17.3830s<br><br>
+![result Image 3]("https://github.com/FabianHen/ImageBuilder/blob/main/Test/Test1/result3.png")<br><br>
+![result Calculation Image 3]("https://github.com/FabianHen/ImageBuilder/blob/main/Test/Test1/result3_calc.png")
