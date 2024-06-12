d024fddb3271cc00332944dd1e55609866048c70
Fabian H <hensslerfabian@gmail.com>
2024-06-12 13:23:42 +0200

    change behaviour of code

--- ImageBuilder.py
+++ ImageBuilder.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import time
+import math
 
 import numpy as np
 from PIL import Image
@@ -10,23 +11,100 @@ from PIL import Image
 ANSIRED = '\033[91m'
 STANDARD = '\033[0m'
 ANSIGREEN = '\033[92m'
+# Threshhold constant that determines when he same Image is used for surrounding pixels
+THRESHOLD = 10
+# 2 Dimensional Array with values that are either 0 or 1, to track if a pixel was already filled
+check_array: list[list[int]]
+# 2 Dimensional Array with RGB values in a tuple, to avoid calculating the values of a pixel twice
+value_array: list[list[int]]
+# The image that is given by the user
+input_image: Image.Image
+# The image that is created
+output_image: Image.Image
+# A list that contains all Images from the given folder together with their average RGB values like this: [(R,G,B), Image]
+image_list: list[tuple[tuple[int, int, int], Image.Image]]
+# The width and heigh of each pixel
+width: int
+height: int
+# The amount of pixels on each side
+amount: int
 
-def green() -> str:
-    return ANSIGREEN + "█" + STANDARD
 
-def red() -> str:
-    return ANSIRED + "█" + STANDARD
+def find_same_color(x: int, y: int, rgb: tuple[int, int, int], image=Image.Image):
+    global input_image, output_image, value_array, check_array, image_list
+    output_image.paste(image, (x * width, y * height))
+    check_array[y][x] = 1
+    if x != 0 and check_array[y][x-1] == 0:
+        color_left = get_pixel_color(y, x-1)
+        offset = calculate_offset(color_left, rgb)
+        if offset < THRESHOLD:
+            find_same_color(x-1, y, rgb, image)
+    if y != amount-1 and check_array[y + 1][x] == 0:
+        color_bottom = get_pixel_color(y + 1, x)
+        offset = calculate_offset(color_bottom, rgb)
+        if offset < THRESHOLD:
+            find_same_color(x, y + 1, rgb, image)
+    if x != amount-1 and check_array[y][x+1] == 0:
+        color_right = get_pixel_color(y, x+1)
+        offset = calculate_offset(color_right, rgb)
+        if offset < THRESHOLD:
+            find_same_color(x+1, y, rgb, image)
 
-def find_picture(rgb, imageList) -> Image:
+
+def calculate_offset(rgb1: tuple[int, int, int], rgb2: tuple[int, int, int]) -> int:
+    off_r = abs(rgb1[0] - rgb2[0])
+    off_g = abs(rgb1[1] - rgb2[1])
+    off_b = abs(rgb1[2] - rgb2[2])
+    return off_r + off_g + off_b
+
+
+def get_pixel_color(x: int, y: int) -> tuple[int, int, int]:
+    global width, height, value_array
+    if value_array[y][x] is None:
+        left = int(x * width)
+        upper = int(y * height)
+        right = int((x + 1) * width)
+        lower = int((y + 1) * height)
+        box = (left, upper, right, lower)
+        # Geting the current pixel from the overall image
+        pixel = input_image.crop(box)
+        value_array[y][x] = tuple(np.array(pixel).mean(axis=(0, 1)))
+    return value_array[y][x]
+
+
+def find_picture(rgb) -> Image:
     best = (None, 765)
-    for ((x, y, z), image) in imageList:
+    for ((x, y, z), image) in image_list:
         # Searches for the Image witth the lowest offset of RGB values
         offset = abs(rgb[0] - x) + abs(rgb[1] - y) + abs(rgb[2] - z)
         if offset < best[1]:
             best = (image, offset)
     return best[0]
 
-def create_image_list(folder) -> list[tuple[tuple, Image.Image]]:
+
+def fill(x: int, y: int):
+    global width, height
+
+    pixelColor = get_pixel_color(x, y)
+    pixelImage: Image = find_picture(pixelColor)
+    pixelImage = pixelImage.resize((width, height))
+    find_same_color(x, y, pixelColor, pixelImage)
+
+
+def create_image(amount: int):
+    global output_image, check_array
+    # Vertical loop
+    for y in range(0, amount):
+        # Horizontal loop
+        for x in range(0, amount):
+            if (check_array[y][x] == 0):
+                print("NEXT\n")
+                fill(x, y)
+    output_image.show()
+
+
+def create_image_list(folder) -> None:
+    global image_list
     image_list = []
     # Only accept certain Image file formats
     filetypes = ("png", "jpg", "jpeg", "webp")
@@ -40,93 +118,65 @@ def create_image_list(folder) -> list[tuple[tuple, Image.Image]]:
             image_list.append((avg_color, img))
     print("time for creating list", time.time() - start, "seconds")
     print("Length of Imagelist:", len(image_list))
-    return image_list
-
-
-def create_image(amount: int, image: Image, width: float, height: float):
-    img = Image.new('RGB', image.size)
-    imageList = create_image_list(sys.argv[3])
-    lastImage: tuple[Image.Image, tuple[int, int, int]] = (None, (0, 0, 0))
-    first: bool = True
-    # Vertical loop
-    for i in range(0, amount):
-        jRange = None
-        # Creating a "Zick-Zack" for the horizontal loop
-        if i % 2 == 1:
-            jRange = range(0, amount)
-        else:
-            jRange = range(amount, 0, -1)
-        statusString: str = ""
-        # Horizontal loop
-        for j in jRange:
-            # Calculating the current pixel
-            left = int(j * width)
-            upper = int(i * height)
-            right = int((j + 1) * width)
-            lower = int((i + 1) * height)
-            box = (left, upper, right, lower)
-            # Geting the current pixel from the overall image
-            pixel = image.crop(box)
-            pixelColor = tuple(np.array(pixel).mean(axis=(0, 1)))
-            # Calculating the RGB offset from the last used Image
-            off_x = abs(lastImage[1][0] - pixelColor[0])
-            off_y = abs(lastImage[1][1] - pixelColor[1])
-            off_z = abs(lastImage[1][2] - pixelColor[2])
-            off = off_x + off_y + off_z
-            # Check if the current Pixels RGB values are significantly different to the last
-            if off > 10 or first:
-                # Find best Image for this Pixel
-                first = False
-                pixelImage: Image = find_picture(pixelColor, imageList)
-                lastImage = (pixelImage, pixelColor)
-                pixelImage = pixelImage.resize((box[2] - box[0], box[3] - box[1]))
-                img.paste(pixelImage, box)
-                statusString = statusString + red()
-            else:
-                # Use last image 
-                resizedLast = lastImage[0].resize((box[2] - box[0], box[3] - box[1]))
-                img.paste(resizedLast, box)
-                statusString = statusString + green()
-        print(statusString)
-    img.show()
-
 
 
 def main():
+    global input_image, output_image, value_array, check_array, image_list, width, height, amount
+
     # Check if input was correct
     if len(sys.argv) < 4:
-        print(ANSIRED + "ERROR: Missing Parameters:", 4 - len(sys.argv), STANDARD)
+        print(ANSIRED + "ERROR: Missing Parameters:",
+              4 - len(sys.argv), STANDARD)
         return
     if not os.path.isfile(sys.argv[1]):
         print(ANSIRED + "ERROR: " + sys.argv[1] + "is not a file!" + STANDARD)
         return
     if not os.path.isdir(sys.argv[3]):
-        print(ANSIRED + "ERROR: " + sys.argv[3] + "is not a directory!" + STANDARD)
+        print(ANSIRED + "ERROR: " +
+              sys.argv[3] + "is not a directory!" + STANDARD)
         return
     im = Image.open(sys.argv[1]).convert('RGB')
     amount = int(sys.argv[2])
-    width, height = im.size
-    pixelWidth = width / amount
-    pixelHeight = height / amount
+    check_array = [[0 for i in range(amount)] for j in range(amount)]
+    value_array = [[None for i in range(amount)] for j in range(amount)]
+    image_width, image_height = im.size
+    print("original:", im.size)
+    width = image_width / amount
+    height = image_height / amount
+    im = im.resize((amount * width, amount * height))
+    print("after:", im.size)
     # Rescale the Image, if the Pixels get to small. Scaling keeps the aspec ratio
     resized: bool = False
-    if pixelWidth < 10:
+    if width < 10:
         resized = True
-        im = im.resize((int(10 * amount), int(10 * amount * height/width)))
+        im = im.resize(
+            (int(10 * amount), int(10 * amount * image_height/image_width)))
         print("Not wide enough - Resized")
-    if pixelHeight < 10:
+    if height < 10:
         resized = True
-        im = im.resize((int(10 * amount * width/height), int(10 * amount)))
+        im = im.resize(
+            (int(10 * amount * image_width/image_height), int(10 * amount)))
         print("Not high enough - Resized")
     if resized:
-        width, height = im.size
-        pixelWidth = width / amount
-        pixelHeight = height / amount
+        image_width, image_height = im.size
+        width = image_width / amount
+        height = image_height / amount
+    width = math.ceil(image_width / amount)
+    height = math.ceil(image_height / amount)
+    im = im.resize((amount * width, amount * height))
+
+    # Setting global variables
+    input_image = im
+    output_image = Image.new('RGB', input_image.size)
+    create_image_list(sys.argv[3])
+
     print("Creating an Image with", str(amount) + "x" + str(amount) + "(" + str(amount*amount)
           + ")" ' "Pixels":')
-    create_image(amount, im, pixelWidth, pixelHeight)
+    print(width, height)
+    create_image(amount)
     time_needed = time.time() - start
-    print(ANSIGREEN + "Process finished after", time_needed, "seconds" + STANDARD)
+    print(ANSIGREEN + "Process finished after",
+          time_needed, "seconds" + STANDARD)
 
 
 start = time.time()
